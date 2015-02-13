cb3f928f5d70eaca7ba2a904313503b2d3b0db96
Julien Sagot <ju.sagot@gmail.com>
2015-02-13 09:07:10 +0100

    Automaton file passed to program with --automaton option.

--- src/cli.ml
+++ src/cli.ml
@@ -5,11 +5,27 @@ open Coverules.Parser.Menhir
 
 let _ =
 
-  let args = Sys.argv |> Array.to_list |> List.tl in
-  let automaton = automaton (Lexing.from_channel stdin) in
-  let traces = List.map
-                 (fun x -> trace (Lexing.from_channel (open_in x)))
-                 args in
+  let af = ref "" in (* automaton file *)
+  let tf = ref [] in (* trace files *)
+
+  let usage = "coverules --automaton FILE [FILES]" in
+
+  let options =
+    Arg.align [
+        "--automaton", Arg.Set_string af,
+        " Mark next file as the automaton to be used." ;
+      ] in
+
+  Arg.parse options (fun s -> tf := s :: !tf) usage ;
+
+  let af = !af in
+  let tf = !tf in
+
+  let lex f = Lexing.from_channel (open_in f) in
+
+  let automaton = automaton (lex af) in
+  let traces = List.map (fun f -> trace (lex f)) tf in
+
   let visited = visited traces automaton |> List.flatten in
   Printf.printf "%d/%d automaton transitions visited.\n"
                 (List.length (List.filter (fun x -> x) visited))
